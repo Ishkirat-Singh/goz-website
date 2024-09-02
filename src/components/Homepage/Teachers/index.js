36ec1096106ef057babd7be14e6e3ea8eed308ac
ayam04 <ayamullahkhan04@gmail.com>
2024-09-03 01:27:16 +0530

    upd

--- src/components/Homepage/Teachers/index.js
+++ src/components/Homepage/Teachers/index.js
@@ -3,13 +3,14 @@ import { Box } from "@mui/system";
 import Image from "next/image";
 import teachers from "./teachers";
 import Marquee from "react-fast-marquee";
+import FloralBorder from "./images/floral-border.png"; // Adjust the path as necessary
 
 const Teachers = () => {
   return (
     <Stack position="relative">
       <Box
         sx={{
-          width: "100%", // Use the full width of the screen
+          width: "100%",
           my: 10,
           overflow: "hidden",
         }}
@@ -30,24 +31,67 @@ const Teachers = () => {
           </Typography>
         </Stack>
 
-        <Marquee
-          gradient={true}
-          speed={60}
-          pauseOnHover
-        >
+        <Marquee gradient={true} speed={70}>
           {teachers.map((teacher, index) => (
             <Stack
               key={index}
               alignItems="center"
               mx={4}
-              sx={{ minWidth: 200 }}
+              sx={{ minWidth: 230 }}
+              position="relative"
             >
-              <Image
-                src={teacher.pic}
-                alt={teacher.name}
-                width={200}
-                height={200}
-              />
+              <Box
+                sx={{
+                  width: 220,
+                  height: 220,
+                  position: "relative",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  sx={{
+                    width: 180,
+                    height: 180,
+                    borderRadius: "50%",
+                    overflow: "hidden",
+                    position: "relative",
+                    zIndex: 1, // Ensure the image is behind the border
+                  }}
+                >
+                  <Image
+                    src={teacher.pic}
+                    alt={teacher.name}
+                    width={200}
+                    height={200}
+                    style={{ borderRadius: "50%" }}
+                  />
+                </Box>
+
+                {/* Floral Border */}
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    zIndex: 2, // Bring the border in front
+                  }}
+                >
+                  <Image
+                    src={FloralBorder}
+                    alt="Floral Border"
+                    layout="fill"
+                    objectFit="contain"
+                    style={{
+                      borderRadius: "50%",
+                      pointerEvents: "none",
+                    }}
+                  />
+                </Box>
+              </Box>
               <Typography variant="h6" sx={{ mt: 2, fontWeight: 600 }}>
                 {teacher.name}
               </Typography>
--- src/components/Homepage/Teachers/teachers.js
+++ src/components/Homepage/Teachers/teachers.js
@@ -1,26 +1,48 @@
-import ManvinderPic from "./images/manvinder-kaur.png";
-import MeenakshiPic from "./images/meenakshi.png";
-import AnamikaPic from "./images/anamika.png";
+import AshimaPic from "./images/ashima.jpg";
+import BhartiPic from "./images/bharti.jpg";
+import BhawnaPic from "./images/bhawna.jpg";
+import DiptiPic from "./images/dipti.jpg";
+import GunjalPic from "./images/gunjal.jpg";
+import KusumPic from "./images/kusum.jpg";
+import MeetaPic from "./images/meeta.jpg";
+import SikhwalPic from "./images/sikhwal.jpg";
+import TruptiPic from "./images/trupti.jpg";
 
 export default [
-  {
-    name: "Ms. Manvinder Kaur",
-    pic: ManvinderPic,
-    school: "ABCE"
-  },
-  {
-    name: "Ms. Manvinder Kaur",
-    pic: ManvinderPic,
-    school: "EFG"
-  },
-  {
-    name: "Mrs. Anamika Shekhar",
-    pic: AnamikaPic,
-    school: "XYZ"
-   },
-  {
-    name: "Dr. Meenakshi Pahuja",
-    pic: MeenakshiPic,
-    school: "KLM"
+    {
+    name: "Ms. Ashima Bhardwaj",
+    pic: AshimaPic,
+    },
+    {
+    name : "Ms. Bharti",
+    pic : BhartiPic,
+    },
+    {
+    name : "Ms. Bhawna Gaba",
+    pic : BhawnaPic,
+    },
+    {
+    name: "Ms. Gunjal Lal",
+    pic: GunjalPic,
+    },
+    {
+    name : "Ms. Meeta Thakker",
+    pic : MeetaPic,
+    },
+    {
+    name : "Dr. KL Sikhwal",
+    pic : SikhwalPic,
+    },
+    {
+      name : "Ms. Trupti Dutta",
+      pic : TruptiPic,
+    },
+    {
+      name : "Ms. Kusum Rani",
+      pic : KusumPic,
+    },
+    {
+      name : "Ms. Dipti Kataria",
+      pic : DiptiPic,
     },
 ];
